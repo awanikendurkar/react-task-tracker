e51a9870215af1eca2581196ffe9671606796e16
Awani <awanikendurkar@gmail.com>
2022-11-26 17:46:08 -0700

    Toggle reminders and style conditionally

--- src/App.js
+++ src/App.js
@@ -26,13 +26,30 @@ function App() {
 
     // delete a task
     const deleteTask = (id) => {
-        console.log("delete", id);
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+
+    // toggle reminder
+    const toggleReminder = (id) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, reminder: !task.reminder } : task
+            )
+        );
     };
 
     return (
         <div className='container'>
             <Header />
-            <Tasks tasks={tasks} onDelete={deleteTask} />
+            {tasks.length > 0 ? (
+                <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                />
+            ) : (
+                "No tasks to show!"
+            )}
         </div>
     );
 }
